c355a3f8318138c490682ed40d0ac8d2e39648b7
lacoperon <elliot.james.williams@gmail.com>
2017-09-26 14:16:45 -0400

    Begun data cleaning; imported data

--- code/0-dataimport.R
+++ code/0-dataimport.R
@@ -1,1 +1,15 @@
 # Initial File for Parliamentary Travel Project
+
+setwd("~/MEGA/TravelersLab/Parliamentary\ Travel/")
+
+library(readr)
+library(dplyr)
+
+boroughs <- read_csv("./data/parliamentary_boroughs_offkey_towns.csv")
+boroughs <- rename(boroughs, parliamentary = "Parliamentary Borough")
+
+pBoroughs <- select(boroughs, parliamentary="X")
+
+setLatLong <- function(df, name, lat, long) {
+  
+}
